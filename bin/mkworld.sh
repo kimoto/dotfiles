f313e25ac3ece46a13e93f25c27915adb16f9d16
U-kimoto-PC\kimoto <kimoto@kimoto-PC.(none)>
2011-06-20 07:33:05 +0900

    modify

--- bin/mkworld.sh
+++ bin/mkworld.sh
@@ -4,10 +4,6 @@ set -x
 
 cd $HOME
 mkdir ./tmp 
-#mkdir ./work
-#mkdir -p ./local
-mkdir -p ./archive/flat
-mkdir -p ./var/log
 
 CURRENT_DIR=`dirname $0`
 . "$CURRENT_DIR/mklink.sh"
---
+++ bin/rmworld.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -x
+
+cd $HOME
+rm "$BASE_DIR/bin" ./
+
+rm ./.vimrc
+rm ./.inputrc
+rm ./.vimperatorrc 
+rm ./.gitconfig
+rm ./.gitignore
+rm ./.screenrc
+rm ./.zshrc
+rm ./.zlogin
+rm ./.bashrc
+rm ./.irbrc
+
+rm ./.vim
+rm ./.subversion
+
+rm ./.Xdefaults
+rm ./.Xmodmap
+
+rm ./.emacs.d
+rm ./.emacs
+
