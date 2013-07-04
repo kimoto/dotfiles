7cc331ae7b3a2b3375ed0c7ba7977deddb450db8
kimoto <sub+peerler@gmail.com>
2013-07-04 19:12:29 +0900

    最適化オプションをO3にしたのと、/tmpで作業するようにした

--- bin/setup_nginx.sh
+++ bin/setup_nginx.sh
@@ -3,6 +3,9 @@
 VER="1.4.1"
 PASSENGER_PATH="`passenger-config --root`/ext/nginx"
 PREFIX="/usr/local/nginx"
+WORKDIR="/tmp"
+
+cd $WORKDIR
 
 wget "http://nginx.org/download/nginx-$VER.tar.gz"
 tar xvfz "./nginx-$VER.tar.gz"
@@ -11,7 +14,7 @@ cd "./nginx-$VER"
 git clone git://gitorious.org/ngx-fancyindex/ngx-fancyindex.git ngx-fancyindex
 sudo yum install -y libcurl-devel pcre-devel gd gd-devel
 
-./configure --prefix="$PREFIX" --with-http_ssl_module --with-cc-opt='-Wno-error -O2' --add-module="$PASSENGER_PATH" --with-http_stub_status_module --add-module=./ngx-fancyindex/ --conf-path="$PREFIX/conf/nginx.conf" 
+./configure --prefix="$PREFIX" --with-http_ssl_module --with-cc-opt='-Wno-error -O3' --add-module="$PASSENGER_PATH" --with-http_stub_status_module --add-module=./ngx-fancyindex/ --conf-path="$PREFIX/conf/nginx.conf" 
 make
 sudo make install
 
@@ -19,7 +22,6 @@ curl -L1 https://gist.github.com/kimoto/3046138/raw/4531d38d660129079146bba6232c
 sudo mv ./nginx /etc/init.d/nginx
 sudo chmod u+x /etc/init.d/nginx
 
-sudo service nginx start
-curl -I http://localhost/
-sudo service nginx stop
+sudo service nginx configtest 
+echo "setup successful!!"
 
