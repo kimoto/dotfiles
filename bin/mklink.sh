8c483000e42668c3e2d712baf5a3b9bcd580faf9
kimoto <your-username@users.noreply.github.com>
2024-09-01 15:55:38 +0900

    Fix

--- bin/mklink.sh
+++ bin/mklink.sh
@@ -6,34 +6,21 @@ BASE_DIR="./dotfiles"
 
 cd $HOME
 ln -sf "$BASE_DIR/bin" ./
+ln -sf ./dotfiles/config ./.config
 
 ln -sf ./dotfiles/.vimrc ./
-ln -sf ./dotfiles/.bvirc ./
 ln -sf ./dotfiles/.inputrc ./
-ln -sf ./dotfiles/.vimperatorrc ./
 ln -sf ./dotfiles/.gitconfig ./
 ln -sf ./dotfiles/.gitignore ./
-ln -sf ./dotfiles/.screenrc ./
+ln -sf ./dotfiles/.tmux.conf ./
 ln -sf ./dotfiles/.zshrc ./
 ln -sf ./dotfiles/.zlogin ./
-ln -sf ./dotfiles/.bashrc ./
 ln -sf ./dotfiles/.irbrc ./
-ln -sf ./dotfiles/.gitconfig ./
 ln -sf ./dotfiles/.gemrc ./
 ln -sf ./dotfiles/.nanorc ./
-ln -sf ./dotfiles/.tmux.conf ./
 
 ln -sf ./dotfiles/.vim ./
 ln -sf ./dotfiles/.zsh ./
-ln -sf ./dotfiles/.subversion ./
-ln -sf ./dotfiles/.peco ./
 
 ln -sf ./dotfiles/.Xdefaults ./
 ln -sf ./dotfiles/.Xmodmap ./
-
-# next generation .emacs!!
-ln -sf ./dotfiles/.emacs.d ./
-ln -sf ./dotfiles/.emacs ./
-
-# $HOMEに./configというシンボリックリンクがなければ作成する
-test -h ./config || ln -sf ./dotfiles ./config
--- bin/mkworld.sh
+++ bin/mkworld.sh
@@ -7,4 +7,4 @@ mkdir ./tmp
 
 CURRENT_DIR=`dirname $0`
 . "$CURRENT_DIR/mklink.sh"
-. "$CURRENT_DIR/mkcrontab.sh"
+# . "$CURRENT_DIR/mkcrontab.sh"
