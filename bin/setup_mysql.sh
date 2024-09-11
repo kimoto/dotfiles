817e813a97addf994bf90719d1d348d7387cba35
kimoto <your-username@users.noreply.github.com>
2024-09-12 06:53:03 +0900

    remove unused files

--- bin/setup_mysql.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-sudo yum install -y mysql-server 
-sudo service mysqld start
-
-/usr/bin/mysqladmin -u root password ''
-/usr/bin/mysql -h localhost -u root -e 'show databases;' # ノーパスワードでアクセスできることを確認
-
--- bin/setup_peercast.in.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-sudo yum install -y libxml2 libxml2-devel libxslt libxslt-devel mysql-devel ImageMagick ImageMagick-devel
-
-cd ./projects/peercast.in
-bundle install
-
