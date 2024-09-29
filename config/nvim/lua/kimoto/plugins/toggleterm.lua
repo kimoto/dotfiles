ecf4339fe078ac5360602bd29019f8f32758b60d
kimoto <your-username@users.noreply.github.com>
2024-09-29 11:18:27 +0900

    improve toggleterm

--- config/nvim/lua/kimoto/plugins/toggleterm.lua
+++ config/nvim/lua/kimoto/plugins/toggleterm.lua
@@ -1,16 +1,6 @@
 require("toggleterm").setup{
   size = 30,
   direction = 'horizontal',
-
+  shade_terminals = true,
+  open_mapping = [[<c-@>]],
 }
-
-local Terminal = require("toggleterm.terminal").Terminal
-local lazygit = Terminal:new({
-	cmd = "lazygit",
-	direction = "float",
-	hidden = true
-})
-
-function lazygit_toggle()
-	lazygit:toggle()
-end
