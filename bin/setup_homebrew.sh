310236b9c20473bc8dc3b7bde8f5cad7a5ab82a0
kimoto <kimoto@users.noreply.github.com>
2025-02-15 10:03:37 +0900

    fix for shellcheck SC2109

--- bin/setup_homebrew.sh
+++ bin/setup_homebrew.sh
@@ -5,7 +5,7 @@ cd "$BASE_DIR" || exit 1;
 
 if ! type "brew" &> /dev/null ; then
     os="$(uname)"
-    if [ "$os" == "Darwin" || "$os" == "Linux" ]; then
+    if [ "$os" == "Darwin" ] || [ "$os" == "Linux" ]; then
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     fi
 fi
