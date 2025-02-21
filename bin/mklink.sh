059bb13931f1cef589fd61a4831855b9a8f9b370
kimoto <kimoto@users.noreply.github.com>
2025-02-21 10:35:45 +0900

    Add default_user gitconfig file to symlink setup

--- bin/mklink.sh
+++ bin/mklink.sh
@@ -13,6 +13,7 @@ ln -nsf "$BASE_DIR/.vim" ./.vim
 ln -sf "$BASE_DIR/.inputrc" ./
 ln -sf "$BASE_DIR/.editrc" ./
 ln -sf "$BASE_DIR/.gitconfig" ./
+ln -sf "$BASE_DIR/.gitconfig.default_user" ./
 ln -sf "$BASE_DIR/.gitignore" ./
 ln -sf "$BASE_DIR/.tmux.conf" ./
 ln -sf "$BASE_DIR/.zshrc" ./
