369788f6ba092369baf0cb506aade6151568e137
kimoto <kimoto@users.noreply.github.com>
2025-03-19 23:10:10 +0900

    emacs keybind

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,3 +1,4 @@
+-- ref: https://qiita.com/naoya@github/items/81027083aeb70b309c14
 local function keyCode(key, modifiers)
    modifiers = modifiers or {}
    return function()
@@ -11,7 +12,32 @@ local function remapKey(modifiers, key, keyCode)
    hs.hotkey.bind(modifiers, key, keyCode, nil, keyCode)
 end
 
+local function disableAllHotkeys()
+   for k, v in pairs(hs.hotkey.getHotkeys()) do
+      v['_hk']:disable()
+   end
+end
+
+local function enableAllHotkeys()
+   for k, v in pairs(hs.hotkey.getHotkeys()) do
+      v['_hk']:enable()
+   end
+end
+
+local function handleGlobalAppEvent(name, event, app)
+   if event == hs.application.watcher.activated then
+      -- hs.alert.show(name)
+      if name ~= "Ghostty" then
+         enableAllHotkeys()
+      else
+         disableAllHotkeys()
+      end
+   end
+end
+
+appsWatcher = hs.application.watcher.new(handleGlobalAppEvent)
+appsWatcher:start()
+
 remapKey({"alt"}, "b", keyCode("left", {"alt"}))
 remapKey({"alt"}, "f", keyCode("right", {"alt"}))
-
 remapKey({"ctrl"}, "w", keyCode("delete", {"alt"}))
